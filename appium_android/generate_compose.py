0c02fb0e90ba61f6d39f0a8da5a0e0c5830cb686
dmitry <dmtrbrlkv@gmail.com>
2022-11-16 19:26:47 +0300

    generate devices compose file

---
+++ appium_android/generate_compose.py
@@ -0,0 +1,58 @@
+import requests
+import os
+from dotenv import load_dotenv
+import pathlib
+
+folder = str(pathlib.Path(__file__).parent.resolve())
+
+load_dotenv()
+
+PUBLIC_IP = os.getenv('PUBLIC_IP')
+STF_TOKEN = os.getenv('STF_TOKEN')
+
+headers = {'Authorization': f'Bearer {STF_TOKEN}'}
+devices = requests.get(f'http://{PUBLIC_IP}/api/v1/devices', headers=headers).json()
+
+docker_compose = """
+version: '2'
+
+services:
+  # Selenium hub
+  selenium_hub:
+    image: selenium/hub:3.14.0-curium
+    ports:
+      - 4444:4444
+"""
+
+device_template = """
+  device_{udid}:
+    build:
+      context: .
+    restart: unless-stopped
+    depends_on:
+      - selenium_hub
+    ports:
+      - {port}:4723
+    privileged: true
+    volumes:
+      - ~/.android:/root/.android
+    environment:
+      - PUBLIC_IP={PUBLIC_IP}
+      - STF_TOKEN={STF_TOKEN}
+      - CONNECT_TO_GRID=true
+      - SELENIUM_HOST=selenium_hub
+      - REMOTE_ADB=true
+      - ANDROID_DEVICES={udid}
+      - REMOTE_ADB_POLLING_SEC=60
+      - RELAXED_SECURITY=true
+"""
+port = 4723
+for device in devices['devices']:
+    if not device['ready']:
+        continue
+    udid = device['serial']
+    docker_compose += device_template.format(udid=udid, PUBLIC_IP=PUBLIC_IP, STF_TOKEN=STF_TOKEN, port=port)
+    port += 1
+
+with open(folder + '/docker-compose.yml', mode='w') as f:
+    f.write(docker_compose)
--- run_devices.sh
+++ run_devices.sh
@@ -1,1 +1,2 @@
-sudo docker-compose -f appium_android/docker-compose.yml up -d --build
+python3 appium_android/generate_compose.py
+sudo docker-compose -f appium_android/docker-compose.yml up -d --build --remove-orphans
