b518072162869e1cd90fddddbfe3a77919e20a14
dmitry <dmtrbrlkv@gmail.com>
2022-11-17 19:39:25 +0300

    генерация конфига в контейнере из готового образа

--- restart_devices.sh
+++ restart_devices.sh
@@ -1,2 +1,2 @@
-docker run --env-file ../.env dmtrbrlkv/gen_devices_compose > appium_android/docker-compose.yml
+docker run --env-file .env dmtrbrlkv/gen_devices_compose > appium_android/docker-compose.yml
 sudo docker-compose -f appium_android/docker-compose.yml up -d --build --remove-orphans
